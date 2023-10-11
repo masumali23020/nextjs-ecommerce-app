b076609354a48915843d34583a20efbc68dc0c33
masumali23020 <mdmasum0218@.com>
2023-10-11 11:55:28 +0600

    authentication with clerk APP

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from "@clerk/nextjs/app-beta";
+
+const SetUpPage = () => {
+  return (
+    <div>
+      SetUpP
+      <UserButton />
+    </div>
+  );
+};
+
+export default SetUpPage;
